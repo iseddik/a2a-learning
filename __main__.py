ea301490096c28c300877ff28b1ce46adf69f0d5
iseddik <seddikissam@outlook.com>
2025-06-23 20:30:21 +0200

    sample a2a

---
+++ __main__.py
@@ -0,0 +1,43 @@
+from a2a.server.tasks import InMemoryTaskStore
+from a2a.types import AgentCapabilities, AgentCard, AgentSkill
+from agent_executor import GreetingAgentExecutor
+from a2a.server.apps import A2AStarletteApplication
+from a2a.server.request_handlers import DefaultRequestHandler
+import uvicorn
+
+def main():
+    skill = AgentSkill(
+        id="hello_world",
+        name="Greet",
+        description="Return a greeting",
+        tags=["greeting", "hello", "world"],
+        example=["Hey", "Hello", "Hi"],
+    )
+
+    agent_card = AgentCard(
+        name="Greeting Agent",
+        description="A simple agent that returns a greeting",
+        url="http://localhost:9999/",
+        defaultInputModes=["text"],
+        defaultOutputModes=["text"],
+        skills=[skill],
+        version="1.0.0",
+        capabilities=AgentCapabilities(),
+    )
+
+    request_handler = DefaultRequestHandler(
+        agent_executor=GreetingAgentExecutor(),
+        task_store=InMemoryTaskStore(),
+    )
+
+    server = A2AStarletteApplication(
+        http_handler=request_handler,
+        agent_card=agent_card,
+    )
+
+    uvicorn.run(server.build(), host="0.0.0.0", port=9999)
+
+
+if __name__ == "__main__":
+    main()
+    print("all is good!")
